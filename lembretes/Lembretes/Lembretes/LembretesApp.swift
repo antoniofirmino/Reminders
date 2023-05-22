0d9467719ef267f2095a5bd51df12fba7edbe0db
Antonio Marcos <87496576+antoniofirmino@users.noreply.github.com>
2023-05-22 10:49:32 -0300

    Add files via upload

---
+++ lembretes/Lembretes/Lembretes/LembretesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LembretesApp.swift
+//  Lembretes
+//
+//  Created by Antonio Marcos on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LembretesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
